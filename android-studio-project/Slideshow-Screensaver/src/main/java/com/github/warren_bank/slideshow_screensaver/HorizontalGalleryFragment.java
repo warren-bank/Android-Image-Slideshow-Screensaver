7f96201e2e287ae203df10c2374f51256f53f5a7
Warren R. Bank <warren.r.bank@gmail.com>
2020-04-02 19:18:26 -0700

    ignore touch, add timer to scroll through images at a fixed interval

--- android-studio-project/Slideshow-Screensaver/src/main/java/com/github/warren_bank/slideshow_screensaver/HorizontalGalleryFragment.java
+++ android-studio-project/Slideshow-Screensaver/src/main/java/com/github/warren_bank/slideshow_screensaver/HorizontalGalleryFragment.java
@@ -1,7 +1,9 @@
 package com.github.warren_bank.slideshow_screensaver;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import androidx.annotation.NonNull;
@@ -52,10 +54,76 @@ public class HorizontalGalleryFragment extends Fragment
         new RecyclerViewPreloader<>(glideRequests, adapter, adapter, 3);
     recyclerView.addOnScrollListener(preloader);
     recyclerView.setAdapter(adapter);
+
+    initSlideshow(adapter);
   }
 
   @Override
   public void onLoaderReset(Loader<List<MediaStoreData>> loader) {
     // Do nothing.
+  }
+
+  private final int delay = 2500;
+  private int       current_image_index;
+  private Handler   handler;
+  private Runnable  slideshow;
+  private boolean   slideshow_running;
+
+  private void initSlideshow(RecyclerAdapter adapter) {
+    if (adapter == null) {
+      slideshow_running = false;
+      return;
+    }
+
+    current_image_index = -1;
+    handler             = new Handler();
+    slideshow           = new Runnable() {
+      @Override
+      public void run() {
+        int total = adapter.getItemCount();
+        int next  = current_image_index + 1;
+
+        if (next >= total)
+          next = 0;
+
+        if (next != current_image_index) {
+          current_image_index = next;
+          recyclerView.scrollToPosition(current_image_index);
+        }
+
+        handler.postDelayed(this, delay);
+      }
+    };
+
+    recyclerView.addOnItemTouchListener(new RecyclerView.SimpleOnItemTouchListener() {
+      @Override
+      public boolean onInterceptTouchEvent(RecyclerView rv, MotionEvent e) {
+        // consume touch event
+        return true;
+      }
+    });
+
+    handler.post(slideshow);
+    slideshow_running = true;
+  }
+
+  @Override
+  public void onResume () {
+    super.onResume();
+
+    if ((handler != null) && !slideshow_running) {
+      handler.postDelayed(slideshow, delay);
+      slideshow_running = true;
+    }
+  }
+
+  @Override
+  public void onPause () {
+    super.onPause();
+
+    if ((handler != null) && slideshow_running) {
+      handler.removeCallbacks(slideshow);
+      slideshow_running = false;
+    }
   }
 }
