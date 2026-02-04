6b1bee31b40bc46d2d7f086b4000abac62e37d10
Warren R. Bank <warren.r.bank@gmail.com>
2026-02-03 18:34:42 -0800

    [Android 15+] fix edge-to-edge layout enforcement
    see:
    * https://developer.android.com/about/versions/15/behavior-changes-15#edge-to-edge
    * https://medium.com/androiddevelopers/insets-handling-tips-for-android-15s-edge-to-edge-enforcement-872774e8839b
    
    docs:
    * https://developer.android.com/reference/android/view/View#attr_android:fitsSystemWindows
    * https://developer.android.com/reference/android/view/View#setFitsSystemWindows(boolean)

--- android-studio-project/Slideshow-Screensaver/src/main/java/com/github/warren_bank/slideshow_screensaver/SettingsFragment.java
+++ android-studio-project/Slideshow-Screensaver/src/main/java/com/github/warren_bank/slideshow_screensaver/SettingsFragment.java
@@ -2,6 +2,9 @@ package com.github.warren_bank.slideshow_screensaver;
 
 import android.os.Bundle;
 import android.preference.PreferenceFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class SettingsFragment extends PreferenceFragment {
   @Override
@@ -9,5 +12,16 @@ public class SettingsFragment extends PreferenceFragment {
     super.onCreate(savedInstanceState);
 
     addPreferencesFromResource(R.xml.preferences);
+  }
+
+  @Override
+  public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    View view = super.onCreateView(inflater, container, savedInstanceState);
+
+    // fix for Android 15+ edge-to-edge layout enforcement
+    if (view != null)
+      view.setFitsSystemWindows(true);
+
+    return view;
   }
 }
